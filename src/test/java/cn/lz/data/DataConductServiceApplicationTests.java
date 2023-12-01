2816eb1472ec6572a5f3909814e8c42c60242089
zhanglei <vip.p@live.com>
2023-12-01 17:51:02 +0800

    2023-12-01：工程创建

---
+++ src/test/java/cn/lz/data/DataConductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lz.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
