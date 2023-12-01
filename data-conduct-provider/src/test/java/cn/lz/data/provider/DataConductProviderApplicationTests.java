d05424b84f5e13e09be1b3f467e5c26040b94a08
zhanglei <vip.p@live.com>
2023-12-01 18:09:00 +0800

    2023-12-01：框架搭建

---
+++ data-conduct-provider/src/test/java/cn/lz/data/provider/DataConductProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lz.data.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConductProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
